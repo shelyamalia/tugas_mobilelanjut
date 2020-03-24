c069f7563c1ef762bef47838c60a29ff156c7ba6
Satria <satriarhamadhan@gmail.com>
2020-03-24 11:10:31 +0700

    TUGAS

---
+++ app/src/main/java/com/teknokrat/mobile2019/ti17a17313059/tugas_mobilelanjut/MainActivity.java
@@ -0,0 +1,14 @@
+package com.teknokrat.mobile2019.ti17a17313059.tugas_mobilelanjut;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
